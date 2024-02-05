9b26740adbc69956aeec5418b868521b074f1bbc
Lashi <lashi0812@gmail.com>
2024-02-05 11:14:08 +0000

    subprocess

---
+++ playground/reaction_game.py
@@ -0,0 +1,13 @@
+
+from time import perf_counter, sleep
+from random import random
+
+print("Press enter to play")
+input()
+print("Ok, get ready!")
+sleep(random() * 5 + 1)
+print("go!")
+start = perf_counter()
+input()
+end = perf_counter()
+print(f"You reacted in {(end - start) * 1000:.0f} milliseconds!\nGoodbye!")
